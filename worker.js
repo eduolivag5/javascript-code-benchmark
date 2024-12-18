84641a58a004e5fcd15be4830603e5c812ffad4b
eduolivag5 <eduolivag5@gmail.com>
2024-12-18 20:41:35 +0100

    first commit

---
+++ worker.js
@@ -0,0 +1,26 @@
+onmessage = async event => {
+    
+    const { code, data, duration } = event.data;
+    
+    let result
+    try {
+        result = await eval(`(async () => {
+            let PERF__ops = 0;
+            let PERF__start = Date.now();
+            let PERF__end = Date.now() + ${duration};
+            ${data}
+
+            while (Date.now() < PERF__end) {
+                ${code}    
+                PERF__ops++;
+            }
+
+            return PERF__ops;
+        })()`)
+    } catch (error) {
+        result = 0;
+        console.log(error)
+    }
+
+    postMessage(result);
+}
